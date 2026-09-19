66d9de66e57c091d0d56dac8a998459b8c97270a
anjepol <japonangel900@gmail.com>
2026-09-18 20:53:27 -0600

    Actualizar sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const V='reproductor-v1',SHELL=['./','index.html','manifest.webmanifest','icon-192.png','icon-512.png'];
+const V='reproductor-v3',SHELL=['./','index.html','manifest.webmanifest','icon-192.png','icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(V).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==V).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
