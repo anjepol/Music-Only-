a7b5568184916582f3c5a95b3561d04715fa5e4c
anjepol <japonangel900@gmail.com>
2026-09-18 20:23:15 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,11 @@
+const V='reproductor-v1',SHELL=['./','index.html','manifest.webmanifest','icon-192.png','icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(V).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==V).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{
+  const r=e.request,u=new URL(r.url);
+  if(r.method!=='GET'||!(u.origin===location.origin||/fonts\.(googleapis|gstatic)\.com$/.test(u.hostname)))return;
+  e.respondWith(caches.match(r).then(hit=>{
+    const net=fetch(r).then(res=>{if(res.ok||res.type==='opaque')caches.open(V).then(c=>c.put(r,res.clone()));return res}).catch(()=>hit);
+    return hit||net;
+  }));
+});
